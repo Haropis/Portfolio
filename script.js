b23d0b72bf5ef470e28f7cd6687c2b640d2907ab
Haropis <haropis.inc@gmail.com>
2023-08-28 17:16:47 +0100

    navbar issues. dealt with tribar menu dropdown for mo bile

---
+++ script.js
@@ -0,0 +1,8 @@
+function dropdownMenu() {
+    var x = document.getElementById("navMenu");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {    
+        x.className = "navbar";
+    }
+}
